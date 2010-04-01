fa070f0a580f5f2688cb28d1f9928b5d075263d3
tgastine <tgastine@758e9eed-5a3c-480c-97ce-78dfd25b0fa2>
2010-04-01 20:42:38 +0000

    add

--- pyQPCR/mainWindow.py
+++ pyQPCR/mainWindow.py
@@ -859,7 +859,7 @@ class Qpcr_qt(QMainWindow):
         import platform
         QMessageBox.about(self, "About pyQPCR",
         """<b>pyQPCR</b> v %s
-        <p>Copyright &copy; 2008- Thomas Gastine - Magali Hennion
+        <p>Copyright &copy; 2008-2010 Thomas Gastine - Magali Hennion - 
         All rights reserved.
         <p>This application can be used to perform
         simple qPCR analysis.
